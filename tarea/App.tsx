b2fcf0334a5cf3ab1605dfe2e45ba270c1f003f0
DESKTOP-14CTLU2\Dell <julliomoya87@gmail.com>
2026-02-11 13:33:21 -0600

    Ejercicio 2 - Implementacion de contador con useState - Comportamiendo condicionado Par/impar - estilizacion de texto si es par/impar

--- tarea/App.tsx
+++ tarea/App.tsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import CustomInput from "./src/components/CustomInput";
+import CustomButton from "./src/components/CustomButtons";
 
 export default function App() {
 
-  // Estado usuario (nombre y edad)
+  // Ejercicio 1-Estado usuario (nombre y edad)
   const [usuario, setUsuario] = useState({
     nombre: "",
     edad: ""
   });
 
-  // Validar si la edad es numero
+  // Ejercicio 1 - Validar si la edad es numero
   const esNumero = (text: string) => {
     return /^\d+$/.test(text);
   };
+
+  // Ejercicio 2 - Contador
+  const [contador, setContador] = useState(0);
 
   return (
     <View style={styles.container}>
@@ -50,6 +54,34 @@ export default function App() {
         </Text>
       )}
 
+      {/* Ejercicio 2 -Contador */}
+      <Text style={styles.counter}>Contador: {contador}</Text>
+
+      {/*Mensaje si es negativo*/}
+      {contador < 0 && (
+        <Text style={styles.warning}>El contador no puede ser negativo</Text>
+      )}
+
+      {/* Par o impar */}
+      <Text
+        style={[
+          styles.parImpar,
+          contador % 2 === 0 ? styles.par : styles.impar
+        ]}
+      >
+        {contador % 2 === 0 ? "Par" : "Impar"}
+      </Text>
+      {/* Botones  */}
+      <CustomButton
+        title="Incrementar"
+        onClick={() => setContador(contador + 1)}
+      />
+      <CustomButton
+        title="Disminuir"
+        onClick={() => setContador(contador - 1)}
+        variant="secondary"
+      />
+
     </View>
   );
 }
@@ -74,5 +106,24 @@ const styles = StyleSheet.create({
     marginTop: 20,
     color: "red",
     fontSize: 16
+  },
+  counter: {
+    fontSize: 32,
+    marginTop: 10
+  },
+  warning: {
+    color: "red",
+    marginTop: 5
+  },
+  parImpar: {
+    fontSize: 18,
+    marginTop: 10
+  },
+  par: {
+    color: "green"
+  },
+  impar: {
+    color: "red"
   }
+
 });
